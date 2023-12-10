2785c80fecf089aed46b8098c59620c5cd05f3f6
Veronica RG <vero.rg0416@gmail.com>
2023-12-10 15:38:53 -0300

    feat: setup date-fns

--- src/languages/i18next.ts
+++ src/languages/i18next.ts
@@ -3,22 +3,28 @@ import i18n from 'i18next';
 
 import { initReactI18next } from 'react-i18next';
 import { INITIAL_LANGUAGE } from '@src/constants';
+import { ptBR as PT_BR_DATE, enUS as EN_US_DATE } from 'date-fns/locale';
 
 import ptBR from './locales/pt-BR.json';
 import enUS from './locales/en-US.json';
 
-export const resources = {
+export const RESOURCES = {
   [INITIAL_LANGUAGE]: ptBR,
   ['en-US']: enUS,
 } as const;
 
+export const DATE_LOCALES = {
+  [INITIAL_LANGUAGE]: PT_BR_DATE,
+  ['en-US']: EN_US_DATE,
+};
+
 i18n.use(initReactI18next).init({
   fallbackLng: [INITIAL_LANGUAGE],
-  ns: Object.keys(resources[INITIAL_LANGUAGE]) as Array<keyof typeof ptBR>,
+  ns: Object.keys(RESOURCES[INITIAL_LANGUAGE]) as Array<keyof typeof ptBR>,
   fallbackNS: Object.keys(ptBR) as Array<keyof typeof ptBR>,
   lng: INITIAL_LANGUAGE,
   debug: false,
-  resources,
+  resources: RESOURCES,
 });
 
 export { i18n };
---
+++ src/utils/formatters/date.ts
@@ -0,0 +1,20 @@
+import { DATE_LOCALES } from '@src/languages';
+import { format } from 'date-fns';
+import i18next from 'i18next';
+
+type DateFormats = '[day] [month] [year]';
+
+type FormatDateParams = { date?: string; format: DateFormats };
+export const formatDate = ({ date: stringDate, format: type }: FormatDateParams) => {
+  if (!stringDate) return '';
+  const date = new Date(stringDate);
+
+  switch (type) {
+    case '[day] [month] [year]':
+      return format(date, 'd [1] MMMM [1] yyyy', {
+        locale: DATE_LOCALES[i18next.t('common:locale') as keyof typeof DATE_LOCALES],
+      }).replaceAll('[1]', i18next.t('common:from'));
+    default:
+      return stringDate;
+  }
+};
